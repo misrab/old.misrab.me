057c10449743b69258dfd16d9610d2a573cc5ac0
Misrab Faizullah-Khan <faizullah.misrab@gmail.com>
2014-01-21 21:21:11 -0800

    version1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,9 @@
 $(function() {
 	$('.carousel').carousel({ interval: false });
+	
+	// when linking to slide from other button, need to select radio
+	$('.carousel').on('slid.bs.carousel', function () {
+		var num = $('.item.active').attr('data-id');
+		$('label[data-slide-to='+num+']').click();
+	});
 });
